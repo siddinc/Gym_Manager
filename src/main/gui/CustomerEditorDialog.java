313c3f5e2660187e5172567d70eed594e1613823
Vikrant Gajria <vikrantgajria@gmail.com>
2018-10-02 13:32:16 +0530

    Added Customer add window.

---
+++ src/main/gui/CustomerEditorDialog.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javax.swing.*;
+
+public class CustomerEditorDialog {
+    private JPanel rootPanel;
+    private JPanel biodataPanel;
+    private JPanel paymentPanel;
+    private JPanel packagePanel;
+    private JLabel firstNameLabel;
+    private JLabel lastNameLabel;
+    private JTextField dobTextField;
+    private JLabel genderLabel;
+    private JTextField genderTextField;
+    private JLabel dobLabel;
+    private JLabel packageLabel;
+    private JList daysPackage;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JLabel paymentLabel;
+    private JTextField textField1;
+    private JButton submitButton;
+
+    private void createUIComponents () {
+        // TODO: place custom component creation code here
+    }
+}
